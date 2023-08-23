58972f7b20406e0d4eb357c0970524add5aab942
eemiljka <eemiljka@metropolia.fi>
2023-08-23 14:21:20 +0300

    second assignment done

---
+++ Assignments/2/2.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const firstCoordinateX = +prompt('Enter the first X-coordinate:');
+const firsttCoordinateY = +prompt('Enter the first Y-coordinate:');
+
+const secondCoordinateX = +prompt('Enter the second X-coordinate:');
+const secondCoordinateY = +prompt('Enter the second Y-coordinate:');
+
+const distance = Math.sqrt(
+  Math.pow(secondCoordinateX - firstCoordinateX, 2) +
+    Math.pow(secondCoordinateY - firsttCoordinateY, 2)
+);
+
+document.getElementById('target').innerHTML = distance;
