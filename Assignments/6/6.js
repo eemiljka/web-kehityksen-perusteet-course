afb2ab34e27d53c348e56a88f318917a0836ac12
eemiljka <eemiljka@metropolia.fi>
2023-08-23 19:58:45 +0300

    assignment 7 almost works

--- Assignments/6/6.js
+++ Assignments/6/6.js
@@ -2,3 +2,12 @@
 
 const naturalNumber = prompt('Enter a positive integer:');
 const naturalnumberInt = parseInt(naturalNumber);
+
+for (let i = 1; i <= naturalnumberInt; i++) {
+  for (let j = 1; j <= naturalnumberInt; j++) {
+    let output = i * j;
+    document.getElementById('target').innerHTML =
+      i + ' x ' + j + ' = ' + output;
+    console.log(i + ' x ' + j + ' = ' + output);
+  }
+}
