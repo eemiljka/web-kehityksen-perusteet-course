b941adaa2132d6c3f489f4a28d9d8a9072749c32
eemiljka <eemiljka@metropolia.fi>
2023-08-26 17:44:13 +0300

    folder change

---
+++ Assignments/Arrays and functions/1/1.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
--- Assignments/Arrays and functions/2/2.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const fruits = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
-
-for (let i = 0; i < fruits.length; i++) {
-  console.log(fruits[i]);
-}
