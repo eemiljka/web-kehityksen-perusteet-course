943877fa740c3c671da2618cfeb23bcab3c4d165
eemiljka <eemiljka@metropolia.fi>
2023-08-30 15:30:59 +0300

    assignment 1 done on event handling

--- Assignments/Event handling/event-assignments/t1/t1.js
+++ Assignments/Event handling/event-assignments/t1/t1.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // array for todo list
 const todoList = [
   {
@@ -28,3 +30,64 @@ const todoList = [
 ];
 
 // add your code here
+
+const ul = document.querySelector('ul');
+
+function makeList() {
+  ul.innerHTML = '';
+  for (const [i, todo] of todoList.entries()) {
+    console.log(i);
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    const label = document.createElement('label');
+    const delButton = document.createElement('button');
+
+    input.type = 'checkbox';
+    input.id = 'todo-' + todo.id;
+    input.checked = todo.completed;
+
+    input.addEventListener('change', function (evt) {
+      todo.completed = input.checked;
+      console.log(todoList);
+    });
+
+    label.htmlFor = 'todo-' + todo.id;
+    label.innerText = todo.task;
+
+    delButton.innerHTML = '&#x2A09';
+    delButton.addEventListener('click', function () {
+      ul.removeChild(li);
+      todoList.splice(i, 1);
+      console.log(todoList);
+    });
+
+    li.appendChild(input);
+    li.appendChild(label);
+    li.appendChild(delButton);
+    ul.appendChild(li);
+  }
+}
+
+makeList();
+
+const addButton = document.querySelector('#add');
+const dialog = document.querySelector('dialog');
+const form = document.querySelector('form');
+
+addButton.addEventListener('click', function () {
+  dialog.showModal();
+});
+
+form.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  const text = document.querySelector('dialog input').value;
+  const item = {
+    id: todoList[todoList.length - 1].id + 1,
+    task: text,
+    completed: false,
+  };
+  todoList.push(item);
+  console.log(todoList);
+  makeList();
+  dialog.close();
+});
