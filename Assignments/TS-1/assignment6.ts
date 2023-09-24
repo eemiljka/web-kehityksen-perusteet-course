a095eff069d04505c64315b7b60a4c1b9523c367
eemiljka <eemiljka@metropolia.fi>
2023-09-24 19:44:28 +0300

    flexbox done

--- Assignments/TS-1/assignment6.ts
+++ Assignments/TS-1/assignment6.ts
@@ -1,30 +1,19 @@
-// TODO: Write interface Item
-// TODO: Define the properties 'name', 'price', and 'quantity' within the interface
-
-// Create an empty array named 'cart' to store the items
-const cart: Item[] = [];
-
-// TODO: Implement a loop to prompt the user for item details
-// Use 'while' loop to keep prompting until an empty item name is entered
-while (true) {
-  // TODO: Prompt user for item name, price, and quantity
-  const itemName = /* TODO: Get user input */;
-
-  // Break the loop if an empty item name is entered
-  if (itemName === "") {
-    break;
-  }
-
-  const itemPrice = /* TODO: Get user input */;
-  const itemQuantity = /* TODO: Get user input */;
-
-  // Create an item object and add it to the 'cart' array
-  const newItem: Item = { name: itemName, price: itemPrice, quantity: itemQuantity };
-  cart.push(newItem);
+const reverseArray = <T>(taulukko: T[]): T[] => {
+    const kaanetty = taulukko.reverse();
+    return kaanetty;
 }
 
-// Calculate the total cost using the 'map' and 'reduce' functions
-const totalCost = cart.map(item => item.price * item.quantity).reduce((sum, cost) => sum + cost, 0);
+// Test the function with arrays of different types
+const numberArray: number[] = [1, 2, 3, 4, 5];
+const stringArray: string[] = ["apple", "banana", "cherry", "date"];
+const mixedArray: (string | number | boolean)[] = [true, 42, "hello", false];
 
-// Display the total cost to the user
-console.log(`Total cost of the shopping cart: $${totalCost.toFixed(2)}`);
+console.log(reverseArray(numberArray));
+console.log(reverseArray(stringArray));
+console.log(reverseArray(mixedArray));
+
+
+// Use console log to print the result. Desired result:
+// Reversed Array of Numbers: [ 5, 4, 3, 2, 1 ]
+// Reversed Array of Strings: [ 'date', 'cherry', 'banana', 'apple' ]
+// Reversed Mixed Array: [ false, 'hello', 42, true ]
